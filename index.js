6e97cdfb8537b481710bf4cad344e5d61ca1c87c
Gary <gary.xu@miduotek.com>
2016-10-02 17:15:09 +0800

    add presign support

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ import {
 
 const AliyunOSS = NativeModules.AliyunOSSModule;
 
-export let initWithAppKey =function(endpoint,accessKeyId,accessKeySecret) {
-    AliyunOSS.initWithAppKey(endpoint,accessKeyId,accessKeySecret)
+export let initWithAppKey =function(endpoint,accessKeyId,accessKeySecret,securityToken,expiration) {
+    AliyunOSS.initWithAppKey(endpoint,accessKeyId,accessKeySecret,securityToken,expiration)
 }
 export let upload= async function(bucketName, objectKey, uploadFilePath) {
     await AliyunOSS.upload(bucketName, objectKey, uploadFilePath)
+}
+export let presignConstrainURL= async function(bucketName, objectKey) {
+    return await AliyunOSS.presignConstrainURL(bucketName, objectKey)
 }
--- ios/AliyunOSSModule.m
+++ ios/AliyunOSSModule.m
@@ -10,17 +10,37 @@ RCT_EXPORT_MODULE(AliyunOSSModule)
 
 RCT_EXPORT_METHOD(initWithAppKey:(NSString*) endpoint
         accessKeyId:(NSString*) accessKeyId
-        accessKeySecret:(NSString*) accessKeySecret)
+        accessKeySecret:(NSString*) accessKeySecret
+        securityToken:(NSString*) securityToken
+        expiration:(NSString*) expiration)
 {
-    NSLog(@"intiWithAppKey:%@,%@,%@",endpoint,accessKeyId,accessKeySecret);
+    NSLog(@"initWithAppKey:%@,%@,%@",endpoint,accessKeyId,accessKeySecret);
+    /*
     // 明文设置secret的方式建议只在测试时使用，更多鉴权模式请参考后面的访问控制章节
     id<OSSCredentialProvider> credential = [[OSSPlainTextAKSKPairCredentialProvider alloc] initWithPlainTextAccessKey:accessKeyId secretKey:accessKeySecret];
+    */
+    id<OSSCredentialProvider> credential =
+    [[OSSFederationCredentialProvider alloc]
+     initWithFederationTokenGetter:^OSSFederationToken* {
+         // 您需要在这里实现获取一个FederationToken，并构造成OSSFederationToken对象返回
+         // 如果因为某种原因获取失败，可直接返回nil
+         
+         OSSFederationToken* token = [OSSFederationToken new];
+         token.tAccessKey = accessKeyId;
+         token.tSecretKey = accessKeySecret;
+         token.tToken = securityToken;
+         token.expirationTimeInGMTFormat = expiration;
+         return token;
+     }];
+    
     OSSClientConfiguration * conf = [OSSClientConfiguration new];
     conf.maxRetryCount = 3; // 网络请求遇到异常失败后的重试次数
     conf.timeoutIntervalForRequest = 30; // 网络请求的超时时间
     conf.timeoutIntervalForResource = 24 * 60 * 60; // 允许资源传输的最长时间
     client = [[OSSClient alloc] initWithEndpoint:endpoint credentialProvider:credential clientConfiguration:conf];
 }
+
+//presignConstrainURL
 RCT_EXPORT_METHOD(upload:(NSString*)bucketName
         objectKey:(NSString*) objectKey
         uploadFilePath:(NSString*) uploadFilePath
@@ -60,4 +80,26 @@ RCT_EXPORT_METHOD(upload:(NSString*)bucketName
 // 可以等待任务完成
 // [putTask waitUntilFinished];
 }
+
+RCT_EXPORT_METHOD(presignConstrainURL:(NSString*)bucketName
+                  objectKey:(NSString*) objectKey
+                  resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
+{
+    NSLog(@"presignConstrainURL: %@/%@", bucketName, objectKey);
+    OSSTask * task = [client presignConstrainURLWithBucketName: bucketName
+                                        withObjectKey: objectKey
+                                        withExpirationInterval: 5*60];
+    NSString* constrainURL;
+    if (!task.error) {
+        constrainURL = task.result;
+        NSLog(@"presignConstrainURL: %@", constrainURL);
+        resolve(@[constrainURL]);
+    }
+    else {
+        NSLog(@"fail to presignConstrainURL, %@", task.error);
+        resolve(@[[NSNull null]]);
+    }
+}
+
 @end
